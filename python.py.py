025928a9180eefa516230bf35fceb01a391f3f21
gaurav9923304418 <31945008+gaurav9923304418@users.noreply.github.com>
2017-09-14 08:54:37 +0530

    Add files via upload

---
+++ python.py.py
@@ -0,0 +1,17 @@
+#!
+print("hello world")
+#BASIC PROGRAM OF CALCULATION  OF TWO NO.
+a=int(input("enter the first no.:"))
+b=int(input("enter the second no.:"))
+add=a+b
+print("addition of two no. is:",add)
+sum=a-b
+mul=a*b
+div=a/b
+print("submission of two no. is:")
+print("multiplication of two no. is:")
+print("division of two no. is:")
+if(a<=b):
+    print(a,"is less than",b)
+else:
+    print(a,"is greater than",b)
